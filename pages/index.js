09cd3acc0926c2eff341c812ba8282a9ac1c76cd
Bartosz Ciąpała <bartosz.cp@gmail.com>
2020-11-16 12:36:03 +0100

    added favicon icon, change theme color

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 import React from "react";
 import HomePage from "../components/HomePage";
 import Header from "../components/Header";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="app">
-      <header>
-        <Header />
-      </header>
-      <div className="container">
-        <main className="main">
-          <HomePage />
-        </main>
+    <>
+      <Head>
+        <title>Ice and Fire</title>
+        <link rel="icon" href="/favicon1.ico" />
+        <meta
+          data-react-helmet="true"
+          name="theme-color"
+          content="#7a0300"
+        ></meta>
+      </Head>
+      <div className="app">
+        <header>
+          <Header />
+        </header>
+        <div className="container">
+          <main className="main">
+            <HomePage />
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
